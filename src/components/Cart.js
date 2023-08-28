c8a363fe7ee9e8c3e6c8f7f3fe079eb822319d74
sachin barali <sachinbarali41@gmail.com>
2023-08-28 15:20:54 +0545

    cart item added

---
+++ src/components/Cart.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import CartItem from './CartItem'
+
+const Cart = () => {
+    const cartItems = [
+        {
+            product_image: "https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wearables/PC_CategoryCard_379X304_1._SY304_CB614835787_.jpg",
+            product_title: "Branded watch",
+            product_price: 12000
+        },
+        {
+            product_image: "https://m.media-amazon.com/images/I/31NPT3Db0NL._AC_SY200_.jpg",
+            product_title: "Black Earpods",
+            product_price: 20000
+        },
+        {
+            product_image: "https://m.media-amazon.com/images/I/41-dSLW1RVL._AC_SY200_.jpg",
+            product_title: "Samsung Mobile",
+            product_price: 30000
+        },
+        {
+            product_image: "https://m.media-amazon.com/images/I/71JNKFTBSwL._AC_UY218_.jpg",
+            product_title: "Hp laptop",
+            product_price: 150000
+        },
+
+    ]
+  return (
+    <div className="container-fluid">
+        <h2 className="text-center text-muted mt-5">
+            Your Cart Items
+        </h2>
+        <div className="d-flex justify-content-around align-items-center mt-5">
+            <div className="col-md-8">
+                {
+                    cartItems.map((cartItem)=>{
+                        return <CartItem product_image={cartItem.product_image} product_title={cartItem.product_title} product_price={cartItem.product_price} />
+                    })
+                }
+            </div>
+            <div className="col-md-3 shadow-lg p-3">
+                <h5 className="text-dark">
+                    Cart Summary
+                </h5>
+                <hr />
+                <p>Units: &nbsp; 1</p>
+                <p>Total: &nbsp; Rs.&nbsp; 00000</p>
+                <hr />
+                <button className="btn btn-warning">
+                    Checkout
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Cart
---
+++ src/components/CartItem.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+const CartItem = (props) => {
+    let [increment, setIncrement] = useState(1)
+
+    const incrementController=()=>{
+        setIncrement(increment++);
+    }
+
+    const decrementController=()=>{
+        setIncrement(increment--);
+    }
+    return (
+        <>
+            <div className="d-flex align-items-center shadow-lg p-3 rounded-3">
+                <div className="col-md-3">
+                    <img src={props.product_image} alt="watch-image" width='130px' />
+                </div>
+                <div className="col-md-3">
+                    {props.product_title}
+                </div>
+                <div className="col-md-2 text-warning">
+                    Rs&nbsp; {props.product_price}
+                </div>
+                <div className="col-md-3 d-flex">
+                    <div className="col-md-4">
+
+                        <button className="btn btn-danger" onClick={decrementController}>-</button>&nbsp;&nbsp;
+                    </div>
+                    <div className="col-md-4">
+                        <p>{increment}</p>
+                    </div>
+                    <div className="col-md-4">
+
+                        <button className="btn btn-primary" onClick={incrementController}>+</button>
+                    </div>
+                </div>
+                <div className="col-md-1 px-3">
+                    <button className="btn btn-danger p-2">
+                        <i class="fa-solid fa-trash text-white" style={{ fontSize: '25px' }}></i>
+                    </button>
+                </div>
+            </div>
+            <hr />
+        </>
+    )
+}
+
+export default CartItem
